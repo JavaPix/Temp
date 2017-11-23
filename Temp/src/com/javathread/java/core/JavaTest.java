8fa66edf44890ccf709c39170e467805626b6b72
AMMAJI <AMMAJI@Asha>
2017-11-23 22:53:23 +0530

    Add

---
+++ Temp/src/com/javathread/java/core/JavaTest.java
@@ -0,0 +1,10 @@
+package com.javathread.java.core;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my Second program");
+	}
+
+}
