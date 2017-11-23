1e179ab2baf99a01c1116c0f84644b00ab166e9f
AMMAJI <AMMAJI@Asha>
2017-11-23 22:37:58 +0530

    Signed-off-by: AMMAJI <AMMAJI@Asha>

---
+++ Temp/src/com/javathread/java/core/SimplProject.java
@@ -0,0 +1,11 @@
+package com.javathread.java.core;
+
+public class SimplProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing.........");
+				
+	}
+
+}
